13c06c3382041732036881924f9113d57c0bc926
Sef <johnseframos16@gmail.com>
2024-04-27 02:37:03 +0800

    so much updates

--- pre-test/jstoguides.js
+++ pre-test/jstoguides.js
@@ -1,4 +1,11 @@
 if (localStorage.getItem("pretestcomp") === "true") {
-    document.getElementById("guides").disabled = false;
+document.getElementById("guides").disabled = false;
+document.getElementById("post").disabled = false;
+}
+
+function unableToClick(event) {
+    if (event.target.disabled) {
+        alert("You can't access this yet until you've finished the pre-test!");
+    }
 
 }
